60e9b91d57ab379032f22e38b636f7413f666a9e
Alex <bazz3k@ya.ru>
2022-10-07 20:07:23 +0300

    Добавил все

---
+++ Задача41/Program.cs
@@ -0,0 +1,34 @@
+﻿/* Пользователь вводит с клавиатуры число M, потом вводит M чисел ЧЕРЕЗ
+ENTER. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+5
+0
+7
+8
+-2
+-2 -> 2
+
+5
+1
+-7
+567
+89
+223-> 3 */
+
+Console.Clear();
+Console.Write("Сколько чисел будете вводить? ");
+int a = Math.Abs(int.Parse(Console.ReadLine()!));
+Console.Write($"Количество чисел больше 0 -> {InputNum(a)}");
+
+
+int InputNum(int a)
+{   
+    int summ = 0;
+    for (int i = 1; i <= a; i++)
+    {
+        int b = int.Parse(Console.ReadLine()!);
+        if (b > 0)
+            summ++;
+    }
+    return summ;
+}   
---
+++ Задача43/Program.cs
@@ -0,0 +1,32 @@
+﻿/* Напишите программу, которая найдёт точку пересечения двух прямых,
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5) */
+
+Console.Clear();
+Console.WriteLine("Введите b1, k1 и b2, k2 через пробел ");
+string input = Console.ReadLine()!;
+double[] variables = ParseToArray(input);
+
+double b1 = variables[0]; double b2 = variables[2];
+double k2 = variables[3]; double k1 = variables[1];
+
+double resK = k1 - k2;
+double resB = b2 - b1;
+double x = resB / resK;
+double y = k1 * x + b1;
+
+Console.WriteLine($"({x}, {y})");
+
+double[] ParseToArray(string str)
+{
+    string[] stringArray = str.Split(" ");
+    double[] result = new double[stringArray.Length];
+ 
+    for (int i = 0; i < stringArray.Length; i++)
+    {
+        result[i] = double.Parse(stringArray[i]);
+    }
+ 
+    return result;
+}
